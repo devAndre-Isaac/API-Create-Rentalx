bdd2244ab29b8d5dfdb66325c9ed49323d7d2beb
devAndre-Isaac <andreisaac344@gmail.com>
2021-07-04 18:57:08 -0300

    feat: Add Debug in Aplication

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
-import express from "express";
+import express, { request, response } from "express";
 
 const app = express();
+
+app.use(express.json())
 
 app.get("/", (request, response) => {
   return response.json({ msg: "Hello world 🌟" });
 });
+
+app.post("/courses", (request, response)=>{
+  const { name } = request.body
+  
+  return response.json({ name })
+})
 
 app.listen(3333, () => console.log("⚡Server is running"));
