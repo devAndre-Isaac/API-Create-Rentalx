0970c46d1369439b55ac7d6415a47f88b379d567
devAndre-Isaac <andreisaac344@gmail.com>
2021-07-10 21:02:03 -0300

    feat: add a instance for get method (SingletonPattern)

--- src/modules/cars/useCases/listCategories/index.ts
+++ src/modules/cars/useCases/listCategories/index.ts
@@ -2,8 +2,10 @@ import { CategoriesRepository } from "../../repositories/CategoriesRepository";
 import { ListCategoriesController } from "../../useCases/listCategories/ListCategoriesController";
 import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 
-const categoriesRepository = new CategoriesRepository();
+const categoriesRepository = CategoriesRepository.getInstance();
+
 const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository);
+
 const listCategoriesController = new ListCategoriesController(
   listCategoriesUseCase
 );
