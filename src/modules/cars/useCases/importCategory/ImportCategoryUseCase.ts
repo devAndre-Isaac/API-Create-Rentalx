06cbd84b7824643225d6f5a39b670a63950fb5a6
devAndre-Isaac <andreisaac344@gmail.com>
2021-07-13 21:01:54 -0300

    feat: reading the upload data

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,17 +1,45 @@
 import fs from "fs";
 import csvParse from "csv-parse";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
 
 class ImportCategoryUseCase {
-  execute(file: any): void {
-    const stream = fs.createReadStream(file.path);
+  constructor(private categoriesRepository: ICategoriesRepository) {}
 
-    const parseFile = csvParse();
+  loadCategories(file: any): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
 
-    stream.pipe(parseFile);
+      const categories: IImportCategory[] = [];
 
-    parseFile.on("data", async (line) => {
-      console.log(line);
+      const parseFile = csvParse();
+
+      stream.pipe(parseFile);
+
+      parseFile
+        .on("data", async (line) => {
+          const [name, description] = line;
+          categories.push({
+            name,
+            description,
+          });
+        })
+        .on("end", () => {
+          resolve(categories);
+        })
+        .on("error", (err) => {
+          reject(err);
+        });
     });
+  }
+
+  async execute(file: any): Promise<void> {
+    const categories = await this.loadCategories(file);
+    console.log(categories);
   }
 }
 export { ImportCategoryUseCase };
